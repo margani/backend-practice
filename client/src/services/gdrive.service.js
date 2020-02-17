6878691c489b4d4e92f90f8bc49e6fbfc56d2a9a
Hossein Margani <hossein.margani@aqovia.com>
2020-02-17 17:19:18 +0000

    Initial implementation of gdrive service

--- client/src/services/gdrive.service.js
+++ client/src/services/gdrive.service.js
@@ -1,3 +1,36 @@
+import google from "googleapis";
+import config from "../config";
+
 export default GoogleDriveService = {
-  
+  OAuth2Client: {},
+  GDriveService: {},
+  Scopes: ["https://www.googleapis.com/auth/drive"],
+  Authorize() {
+    this.OAuth2Client = new google.auth.OAuth2(
+      config.GDriveClientId,
+      config.GDriveClientSecret,
+      config.GDriveClientRedirectUri
+    );
+
+    this.GDriveService = google.drive({
+      version: "v3",
+      auth: this.OAuth2Client
+    });
+  },
+  isAuthorized() {
+    return Object.keys(this.OAuth2Client.credentials).length > 0;
+  },
+  getAccessToken(code, callback) {
+    var self = this;
+    this.OAuth2Client.getToken(code, (err, token) => {
+      if (err) {
+        console.log("ERROR HERE");
+        return callback(err);
+      }
+
+      self.OAuth2Client.setCredentials(token);
+
+      callback();
+    });
+  }
 };
--- server/model/google_drive_api.js
+++ server/model/google_drive_api.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
+const fs = require("fs");
+const readline = require("readline");
+const { google } = require("googleapis");
 
-const SCOPES = ['https://www.googleapis.com/auth/drive'];
-const TOKEN_PATH = 'credentials.json';
+const SCOPES = ["https://www.googleapis.com/auth/drive"];
+const TOKEN_PATH = "credentials.json";
 
-var credentials={};
+var credentials = {};
 /*
 fs.readFile('client_secret.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
@@ -15,77 +15,76 @@ fs.readFile('client_secret.json', (err, content) => {
   console.log(credentials);
 });
 */
-try{
-	credentials = JSON.parse(fs.readFileSync('./client_secret.json', 'utf8'));
-	}catch (err) {
-        throw err;
-	}
-var authorize = function(credentials){
-	//console.log(credentials);
-	var clientSecret = credentials.installed.client_secret;
-	var clientId = credentials.installed.client_id;
-	var redirectUrl = credentials.installed.redirect_uris[0];
-	const OAuth2Client = google.auth.OAuth2;
-	
-	
-	this.scopes = SCOPES;
-	this.oauth2Client = new OAuth2Client(clientId,clientSecret,redirectUrl);
-	service_drive = google.drive({ version: 'v3', auth: this.oauth2Client });
-	
-	this.isAuthorised = function(){
-		//console.log(this.oauth2Client.credentials);
-		return Object.keys(this.oauth2Client.credentials).length > 0;
-	};
-	
-	/*
-	*	@param {String} authorizationCode
-    *   @param {Function} callback
-	*/
-	this.getAccessToken = function(code, callback){
-		
-		var self =this;
-		this.oauth2Client.getToken(code, (err, token) => {
-			if (err) {
-			console.log("ERROR HERE");
-			return callback(err);
-			}
-				
-			self.oauth2Client.setCredentials(token);
-			
-     
-			callback();
-		});
-	};
-	
-	this.getList = function(ID,callback){
-		service_drive.files.list({
-			auth : this.oauth2Client,
-			pageSize:10,
-			q: "'"+ID+"' in parents and trashed=false",
-			orderBy: 'folder,modifiedTime desc,name'
-		},function(err,res){
-			if(err)
-			return console.log(err);
-			
-			callback(res.data.files)
-		});
-	};
-	
-	
-	this.downloadFile = function(ID,callback){
-		service_drive.files.get({
-			auth : this.oauth2Client,
-			fields : 'webContentLink , id, name,mimeType',
-			fileId: ID
-		},function(err,res){
-			//console.log(res.data);
-			
-			callback(res.data.webContentLink);
-		});
-		
-	};
-	
+try {
+  credentials = JSON.parse(fs.readFileSync("./client_secret.json", "utf8"));
+} catch (err) {
+  throw err;
 }
 
-module.exports = new authorize (credentials);
+var authorize = function(credentials) {
+  //console.log(credentials);
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  const OAuth2Client = google.auth.OAuth2;
 
+  this.scopes = SCOPES;
+  this.oauth2Client = new OAuth2Client(clientId, clientSecret, redirectUrl);
+  service_drive = google.drive({ version: "v3", auth: this.oauth2Client });
+
+  this.isAuthorised = function() {
+    //console.log(this.oauth2Client.credentials);
+    return Object.keys(this.oauth2Client.credentials).length > 0;
+  };
+
+  /*
+   *	@param {String} authorizationCode
+   *   @param {Function} callback
+   */
+  this.getAccessToken = function(code, callback) {
+    var self = this;
+    this.oauth2Client.getToken(code, (err, token) => {
+      if (err) {
+        console.log("ERROR HERE");
+        return callback(err);
+      }
+
+      self.oauth2Client.setCredentials(token);
+
+      callback();
+    });
+  };
+
+  this.getList = function(ID, callback) {
+    service_drive.files.list(
+      {
+        auth: this.oauth2Client,
+        pageSize: 10,
+        q: "'" + ID + "' in parents and trashed=false",
+        orderBy: "folder,modifiedTime desc,name"
+      },
+      function(err, res) {
+        if (err) return console.log(err);
+
+        callback(res.data.files);
+      }
+    );
+  };
+
+  this.downloadFile = function(ID, callback) {
+    service_drive.files.get(
+      {
+        auth: this.oauth2Client,
+        fields: "webContentLink , id, name,mimeType",
+        fileId: ID
+      },
+      function(err, res) {
+        //console.log(res.data);
+
+        callback(res.data.webContentLink);
+      }
+    );
+  };
+};
+
+module.exports = new authorize(credentials);
